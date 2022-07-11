93362605ee29d26b8e84852fad9956933e673892
Mati1432 <mateuszsliwinski9@gmail.com>
2022-07-11 14:56:07 +0200

    register user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,3 +2,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from accounts.models import Users
+
+
+@admin.register(Users)
+class CustomUserAdmin(admin.ModelAdmin):  # noqa D101
+    pass
