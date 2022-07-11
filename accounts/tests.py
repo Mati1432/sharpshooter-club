87505ef25a5ebd0f4931d4545b59f516e3beb8f9
Mati1432 <mateuszsliwinski9@gmail.com>
2022-07-11 14:56:28 +0200

    Test model Users

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,3 +2,28 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import reverse
+
+from accounts.models import Users
+from django.utils import timezone
+
+
+class UserTest(TestCase):
+    def setUp(self):
+        self.users = Users.objects.create(
+            password='abc',
+            last_login=timezone.now(),
+            is_superuser=True,
+            username='Test',
+            first_name='Test',
+            last_name='Test',
+            email='admin@wp.pl',
+            is_staff=True,
+            is_active=True,
+            date_joined=timezone.now(),
+            user='Test'
+        )
+
+    def test_model(self):
+        self.assertEqual(self.users.password, 'abc')
+        self.assertEqual(self.users.last_login, self.users.date_joined)
