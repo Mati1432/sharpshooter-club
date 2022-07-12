0ade8513e882b0871de284c211566ce2a4b67d9c
Mati1432 <mateuszsliwinski9@gmail.com>
2022-07-12 16:55:50 +0200

    create Users model

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,20 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# Create your models here.
-class Users(AbstractUser):
-    user = models.CharField(max_length=45)
+class Users(AbstractUser):  # noqa: D101
+    pesel = models.CharField(max_length=11, blank=True)
+    city = models.CharField(max_length=45, blank=True)
+    postal_code = models.CharField(max_length=6, blank=True)
+    street = models.CharField(max_length=45, blank=True)
+    house_number = models.CharField(max_length=45, blank=True)
+    phone = models.CharField(blank=True, max_length=15)
+    image = models.ImageField(upload_to='Images/', null=True, blank=True)
+    club = models.CharField(max_length=20, blank=True)
+
+    class Meta:  # noqa: D106
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):  # noqa: D105
+
+        return f'{self.first_name} {self.last_name} {self.email}'
