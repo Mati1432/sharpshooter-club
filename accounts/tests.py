edd28eb7bc1250c1d793d68c860dd4a69bc869a1
Mati1432 <mateuszsliwinski9@gmail.com>
2022-07-13 19:02:45 +0200

    Add test to validate form

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,9 +1,14 @@
 # Django
+import secrets
+import string
+
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 
 # Create your tests here.
 from django.urls import reverse
 
+from accounts.forms import SignUpForm
 from accounts.models import Users
 from django.utils import timezone
 
@@ -45,3 +50,33 @@ class UserModelTest(TestCase):
     def test_simple_users_retrieving(self):
         users_get = Users.objects.get(id=self.users.id)
         self.assertEqual(self.users.id, users_get.id)
+
+
+class SignUpFormTest(TestCase):
+    def setUp(self):
+        randomize_password = ''.join((secrets.choice(
+            string.ascii_letters + string.digits)
+            for i in range(20))
+        )
+        upload_file = open(r'C:\Users\Mateusz\Desktop\test.jpg', 'rb')
+
+        self.form_data = {
+            'first_name': 'Name',
+            'last_name': 'Last_name',
+            'email': 'test@test.com',
+            'city': 'City',
+            'pesel': '12345678901',
+            'postal_code': '00-000',
+            'street': 'Street',
+            'house_number': '00',
+            'phone': '+00 000000000',
+            'photo': SimpleUploadedFile(upload_file.name, upload_file.read()),
+            'club': 'Club',
+            'date_brith': '2022-07-13',
+            'password1': randomize_password,
+            'password2': randomize_password,
+        }
+
+    def test_validate_form(self):
+        form = SignUpForm(data=self.form_data)
+        self.assertTrue(form.is_valid())
