db49e8f33747db0165e5ac4f94ff0f835ffe9141
Mati1432 <mateuszsliwinski9@gmail.com>
2022-07-13 19:20:23 +0200

    change test model Users

--- accounts/tests.py
+++ accounts/tests.py
@@ -13,43 +13,57 @@ from accounts.models import Users
 from django.utils import timezone
 
 
-# class UserModelTest(TestCase):
-#     def setUp(self):
-#         self.users = Users.objects.create(
-#             password='abc',
-#             last_login=timezone.now(),
-#             is_superuser=True,
-#             username='Test',
-#             first_name='Test',
-#             last_name='Test',
-#             email='admin@wp.pl',
-#             is_staff=True,
-#             is_active=True,
-#             date_joined=timezone.now(),
-#             user='Test'
-#         )
-#
-#     def test_simple_user_creation(self):
-#         users = self.users
-#         self.assertIsNotNone(users)
-#         self.assertIsInstance(users, Users)
-#         self.assertEqual(users.first_name, 'Test')
-#         self.assertFalse(users.user_permissions.exists())
-#         self.assertFalse(users.groups.exists())
-#
-#     def test_simple_user_update(self):
-#         users = self.users
-#         users.first_name = 'Test2'
-#         users.save()
-#         self.assertEqual(users.first_name, 'Test2')
-#
-#     def test_simple_user_delete(self):
-#         Users.objects.all().delete()
-#         self.assertFalse(Users.objects.exists())
-#
-#     def test_simple_users_retrieving(self):
-#         users_get = Users.objects.get(id=self.users.id)
-#         self.assertEqual(self.users.id, users_get.id)
+class UserModelTest(TestCase):
+    def setUp(self):
+        self.randomize_password = ''.join((secrets.choice(
+            string.ascii_letters + string.digits)
+            for i in range(20))
+        )
+        upload_file = open(r'C:\Users\Mateusz\Desktop\test.jpg', 'rb')
+        self.users = Users.objects.create(
+            password=self.randomize_password,
+            last_login=timezone.now(),
+            is_superuser=True,
+            username='Test',
+            first_name='Name',
+            last_name='Last Name',
+            email='test@test.com',
+            is_staff=True,
+            is_active=True,
+            date_joined=timezone.now(),
+            city='City',
+            pesel='12345678901',
+            postal_code='00-000',
+            street='Street',
+            house_number='00',
+            phone='+00 000000000',
+            photo=SimpleUploadedFile(upload_file.name, upload_file.read()),
+            club='Club',
+            birth_date='2022-07-13',
+
+        )
+
+    def test_simple_user_creation(self):
+        users = self.users
+        self.assertIsNotNone(users)
+        self.assertIsInstance(users, Users)
+        self.assertEqual(users.first_name, 'Name')
+        self.assertFalse(users.user_permissions.exists())
+        self.assertFalse(users.groups.exists())
+
+    def test_simple_user_update(self):
+        users = self.users
+        users.first_name = 'Name2'
+        users.save()
+        self.assertEqual(users.first_name, 'Name2')
+
+    def test_simple_user_delete(self):
+        Users.objects.all().delete()
+        self.assertFalse(Users.objects.exists())
+
+    def test_simple_users_retrieving(self):
+        users_get = Users.objects.get(id=self.users.id)
+        self.assertEqual(self.users.id, users_get.id)
 
 
 class SignUpFormTest(TestCase):
