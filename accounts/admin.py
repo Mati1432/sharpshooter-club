91e5f00d8b3d3835caf382d14a25f01c1df2d025
Mati1432 <mateuszsliwinski9@gmail.com>
2022-07-14 13:59:58 +0200

    set up admin model users

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,10 +3,30 @@
 from django.contrib import admin
 
 # Project
-# Register your models here.
 from accounts.models import Users
 
 
 @admin.register(Users)
-class CustomUserAdmin(admin.ModelAdmin):  # noqa D101
-    pass
+class UsersAdmin(admin.ModelAdmin):  # noqa D101
+    list_display = [
+        'first_name',
+        'last_name',
+        'pesel',
+        'city',
+        'postal_code',
+        'street',
+        'house_number',
+    ]
+    list_filter = [
+        'last_name',
+    ]
+    list_display_links = (
+        'last_name',
+    )
+    list_editable = [
+        'pesel',
+        'city',
+        'postal_code',
+        'street',
+        'house_number',
+    ]
