3a5fd5340b77b1768eaf24e6f9e0ebd2150ff802
Mati1432 <mateuszsliwinski9@gmail.com>
2022-07-18 17:02:11 +0200

    Add three model for user license and achievements

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@
 # Django
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from consts import license_type_consts
 
 
 class Users(AbstractUser):  # noqa: D101
@@ -21,3 +22,45 @@ class Users(AbstractUser):  # noqa: D101
 
     def __str__(self):  # noqa: D105
         return f'{self.first_name} {self.last_name} {self.email}'
+
+
+class AchievementIndividual(models.Model):  # noqa: D101
+    name = models.CharField(max_length=45)
+    date = models.DateField()
+    score = models.DecimalField(max_digits=10, decimal_places=2)
+    users_achievement_individual = models.ForeignKey(Users, on_delete=models.CASCADE, null=False)
+
+    class Meta:  # noqa: D106
+        verbose_name = 'Achievement Individual'
+        verbose_name_plural = 'Achievement Individuals'
+
+    def __str__(self):  # noqa: D105
+        return f'{self.name} {self.score}'
+
+
+class AchievementCompetition(models.Model):  # noqa: D101
+    name = models.CharField(max_length=45)
+    date = models.DateField()
+    score = models.IntegerField()
+    user_achievement_competition = models.ForeignKey(Users, on_delete=models.CASCADE, null=False)
+
+    class Meta:  # noqa: D106
+        verbose_name = 'Achievement Competition'
+        verbose_name_plural = 'Achievement Competitions'
+
+    def __str__(self):  # noqa: D105
+        return f'{self.name} {self.score}'
+
+
+class License(models.Model):  # noqa: D101
+    license_is_active = models.BooleanField(default=False)
+    date = models.DateField()
+    license_type = models.CharField(max_length=150, choices=license_type_consts)
+    user_license = models.ForeignKey(Users, on_delete=models.CASCADE, null=False)
+
+    class Meta:  # noqa: D106
+        verbose_name = 'Achievement Competition'
+        verbose_name_plural = 'Achievement Competitions'
+
+    def __str__(self):  # noqa: D105
+        return f'{self.license_is_active} {self.license_type}'
