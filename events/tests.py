a05d8ddcb53f7ca74aa623471b4eab9f7adb86b9
Mati1432 <mateuszsliwinski9@gmail.com>
2022-07-27 12:03:06 +0200

    add test for competition model

--- events/tests.py
+++ events/tests.py
@@ -8,7 +8,7 @@ from django.test import TestCase
 from django.utils import timezone
 
 # Project
-from events.models import Events
+from events.models import Events, Competition
 
 
 class EventsModelTest(TestCase):  # noqa D101
@@ -51,3 +51,43 @@ class EventsModelTest(TestCase):  # noqa D101
         events_delete = Events.objects.filter(id=self.events.id)
         events_delete.delete()
         self.assertFalse(events_delete.exists())
+
+
+class CompetitionModelTest(TestCase):  # noqa D101
+    def setUp(self):  # noqa D102
+        events_model_test = EventsModelTest()
+        events_model_test.setUp()
+
+        self.competition = Competition.objects.create(
+            name='Name',
+            code='12345',
+            self_weapon_first=23.32,
+            self_weapon_second=12.22,
+            club_weapon_first=7.32,
+            club_weapon_second=5.22,
+            free_space=120,
+            events=events_model_test.events,
+        )
+
+    def test_simple_competition_update(self):  # noqa D102
+        competition = self.competition
+        competition.name = 'Test Name'
+        competition.code = '54321'
+        competition.free_space = 130
+        competition.save()
+
+        self.assertEqual(competition.name, 'Test Name')
+        self.assertEqual(competition.code, '54321')
+        self.assertIsNotNone(competition)
+        self.assertTrue(Competition.objects.filter(events=competition.events).exists())
+        self.assertFalse(Competition.objects.filter(name='Name').exists())
+        self.assertFalse(Competition.objects.filter(code='12345').exists())
+
+    def test_simple_competition_retrieving(self):  # noqa D102
+        competition_get = Competition.objects.get(id=self.competition.id)
+        self.assertEqual(self.competition.id, competition_get.id)
+
+    def test_simple_competition_delete(self):  # noqa D102
+        competition_delete = Competition.objects.filter(id=self.competition.id)
+        competition_delete.delete()
+        self.assertFalse(competition_delete.exists())
